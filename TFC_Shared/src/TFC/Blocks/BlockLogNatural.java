eee27ba20ff07a251ef94dae31ad2d48db28d9af
Marco Cunha <marco.cunha@gmail.com>
2014-01-12 23:38:31 +0000

    Normalize white space

--- TFC_Shared/src/TFC/Blocks/BlockLogNatural.java
+++ TFC_Shared/src/TFC/Blocks/BlockLogNatural.java
@@ -29,24 +29,24 @@ public class BlockLogNatural extends BlockTerra
 	}
 	
 	@Override
-    public void updateTick(World world, int i, int j, int k, Random rand)
-    {
+	public void updateTick(World world, int i, int j, int k, Random rand)
+	{
 		if (world.isRemote) return;
 		
 		if (world.isBlockOpaqueCube(i, j-1, k)) return;
 		
 		if (world.getBlockId(i+1, j, k  ) == blockID || 
-		    world.getBlockId(i-1, j, k  ) == blockID || 
-		    world.getBlockId(i  , j, k+1) == blockID || 
-		    world.getBlockId(i  , j, k-1) == blockID || 
-		    world.getBlockId(i+1, j, k+1) == blockID || 
-		    world.getBlockId(i+1, j, k-1) == blockID || 
-		    world.getBlockId(i-1, j, k+1) == blockID || 
-		    world.getBlockId(i-1, j, k-1) == blockID) 
+			world.getBlockId(i-1, j, k  ) == blockID || 
+			world.getBlockId(i  , j, k+1) == blockID || 
+			world.getBlockId(i  , j, k-1) == blockID || 
+			world.getBlockId(i+1, j, k+1) == blockID || 
+			world.getBlockId(i+1, j, k-1) == blockID || 
+			world.getBlockId(i-1, j, k+1) == blockID || 
+			world.getBlockId(i-1, j, k-1) == blockID) 
 			return;
 		
 		world.setBlock(i, j, k, 0);
-    }
+	}
 
 	@SideOnly(Side.CLIENT)
 	@Override
@@ -92,15 +92,15 @@ public class BlockLogNatural extends BlockTerra
 	public static Icon[] rotatedSideIcons = new Icon[Global.WOOD_ALL.length];
 	
 	@Override
-    public void registerIcons(IconRegister registerer)
-    {
+	public void registerIcons(IconRegister registerer)
+	{
 		for (int i = 0; i < Global.WOOD_ALL.length; i++)
 		{
 			sideIcons[i] = registerer.registerIcon(Reference.ModID + ":" + "wood/trees/" + Global.WOOD_ALL[i] + " Log");
 			innerIcons[i] = registerer.registerIcon(Reference.ModID + ":" + "wood/trees/" + Global.WOOD_ALL[i] + " Log Top");
 			rotatedSideIcons[i] = registerer.registerIcon(Reference.ModID + ":" + "wood/trees/" + Global.WOOD_ALL[i] + " Log Side");
 		}
-    }
+	}
 
 	static int damage = 0;
 	boolean isStone = false;
@@ -131,14 +131,14 @@ public class BlockLogNatural extends BlockTerra
 				break;
 			}
 		}
-//		if (!isAxeorSaw) 
-//		{
-//			for(int cnt = 0; cnt < Recipes.Saws.length; cnt++)
-//			{
-//				if(equip.getItem() == Recipes.Saws[cnt])
-//					isAxeorSaw = true;
-//			}
-//		}
+		//if (!isAxeorSaw) 
+		//{
+		//	for(int cnt = 0; cnt < Recipes.Saws.length; cnt++)
+		//	{
+		//		if(equip.getItem() == Recipes.Saws[cnt])
+		//		isAxeorSaw = true;
+		//	}
+		//}
 		if (!isAxeorSaw) {
 			for (int cnt = 0; cnt < Recipes.Hammers.length; cnt++)
 			{
@@ -173,9 +173,9 @@ public class BlockLogNatural extends BlockTerra
 	
 	@Override
 	public boolean canBlockStay(World par1World, int par2, int par3, int par4)
-    {
-        return true;
-    }
+	{
+		return true;
+	}
 
 	@Override
 	public void onBlockDestroyedByExplosion(World world, int i, int j, int k, Explosion ex) 
@@ -183,112 +183,112 @@ public class BlockLogNatural extends BlockTerra
 		ProcessTree(world, i, j, k, world.getBlockMetadata(i, j, k), null);
 	}
 
-    class TreeScan {
-    	public World world;
-        public int i;
-        public int j;
-        public int k;
-        public int l;
-        public boolean checkArray[][][];
-        public int x;
-        public int y;
-        public int z;
-        public ItemStack stack;
-        public int xLength;
-        public int zLength;
-        public TreeScan(World world, int i, int j, int k, int l, boolean[][][] checkArray,int x, int y, int z, ItemStack stack) {
-        	this.world = world;
-            this.i = i;
-            this.j = j;
-            this.k = k;
-            this.l = l;
-            this.checkArray = checkArray;
-            this.x = x;
-            this.y = y;
-            this.z = z;
-            this.stack = stack;
-            this.xLength = checkArray.length;
-            this.zLength = checkArray[0][0].length;
-        }
-        /**
-         * Recursion helper that creates a new instance with updated coordinates
-         * @return Returns a new scanner with the provided coordinates to help recursion
-         */
-        public TreeScan updateCoords(int i, int j, int k, int x, int y, int z) {
-        	return new TreeScan(this.world, i, j, k, this.l, this.checkArray, x, y, z, this.stack);
-        }
-    }
-    
-    private void ProcessTree(World world, int i, int j, int k, int l, ItemStack stack)
-    {
-    	TreeScan ts = new TreeScan(world, i, j, k, l, new boolean[11][50][11], 6, 0, 6, stack);
-        int x = i;
-        int y = 0;
-        int z = k;
+	class TreeScan {
+		public World world;
+		public int i;
+		public int j;
+		public int k;
+		public int l;
+		public boolean checkArray[][][];
+		public int x;
+		public int y;
+		public int z;
+		public ItemStack stack;
+		public int xLength;
+		public int zLength;
+		public TreeScan(World world, int i, int j, int k, int l, boolean[][][] checkArray,int x, int y, int z, ItemStack stack) {
+			this.world = world;
+			this.i = i;
+			this.j = j;
+			this.k = k;
+			this.l = l;
+			this.checkArray = checkArray;
+			this.x = x;
+			this.y = y;
+			this.z = z;
+			this.stack = stack;
+			this.xLength = checkArray.length;
+			this.zLength = checkArray[0][0].length;
+		}
+		/**
+		 * Recursion helper that creates a new instance with updated coordinates
+		 * @return Returns a new scanner with the provided coordinates to help recursion
+		 */
+		public TreeScan updateCoords(int i, int j, int k, int x, int y, int z) {
+			return new TreeScan(this.world, i, j, k, this.l, this.checkArray, x, y, z, this.stack);
+		}
+	}
+	
+	private void ProcessTree(World world, int i, int j, int k, int l, ItemStack stack)
+	{
+		TreeScan ts = new TreeScan(world, i, j, k, l, new boolean[11][50][11], 6, 0, 6, stack);
+		int x = i;
+		int y = 0;
+		int z = k;
 
-        boolean reachedTop = false;
-       	while (!reachedTop) {
-       		if (world.getBlockId(x, j+y+1, z) == 0) {
-       			if (l != 9 && l != 15) {
-                    reachedTop = true;
-       			} else {
+		boolean reachedTop = false;
+		while (!reachedTop) {
+			if (world.getBlockId(x, j+y+1, z) == 0) {
+				if (l != 9 && l != 15) {
+					reachedTop = true;
+				} else {
 					if (world.getBlockId(x+1, j+y+1, z  ) != blockID && 
 						world.getBlockId(x-1, j+y+1, z  ) != blockID && 
-					    world.getBlockId(x  , j+y+1, z+1) != blockID && 
-					    world.getBlockId(x  , j+y+1, z-1) != blockID && 
-					    world.getBlockId(x-1, j+y+1, z-1) != blockID && 
-					    world.getBlockId(x-1, j+y+1, z+1) != blockID && 
-					    world.getBlockId(x+1, j+y+1, z+1) != blockID && 
-					    world.getBlockId(x+1, j+y+1, z-1) != blockID) {
-		                    reachedTop = true;
-				    }
-       			}
-       		}
-        	y++;
-       	}
-        while (y >= 0) {
-            scanLogs(ts.updateCoords(i,j+y,k,6,y--,6));
-        }
-    }
+						world.getBlockId(x  , j+y+1, z+1) != blockID && 
+						world.getBlockId(x  , j+y+1, z-1) != blockID && 
+						world.getBlockId(x-1, j+y+1, z-1) != blockID && 
+						world.getBlockId(x-1, j+y+1, z+1) != blockID && 
+						world.getBlockId(x+1, j+y+1, z+1) != blockID && 
+						world.getBlockId(x+1, j+y+1, z-1) != blockID) {
+							reachedTop = true;
+					}
+				}
+			}
+			y++;
+		}
+		while (y >= 0) {
+			scanLogs(ts.updateCoords(i,j+y,k,6,y--,6));
+		}
+	}
 
-    private void scanLogs(TreeScan ts)
-    {
-    	if (ts.y < 0) return;
+	private void scanLogs(TreeScan ts)
+	{
+		if (ts.y < 0) return;
 
-        ts.checkArray[ts.x][ts.y][ts.z] = true;
-        int offsetX = 0;int offsetY = 0;int offsetZ = 0;
-        
-        for (offsetX = -2; offsetX <= 2; offsetX++)
-        {
-        	if (ts.x+offsetX >= ts.xLength || ts.x+offsetX < 0) continue;
+		ts.checkArray[ts.x][ts.y][ts.z] = true;
+		int offsetX = 0;int offsetY = 0;int offsetZ = 0;
+		
+		for (offsetX = -2; offsetX <= 2; offsetX++)
+		{
+			if (ts.x+offsetX >= ts.xLength || ts.x+offsetX < 0) continue;
 
-            for (offsetZ = -2; offsetZ <= 2; offsetZ++)
-            {
-            	if (ts.z+offsetZ >= ts.zLength || ts.z+offsetZ < 0) continue;
+			for (offsetZ = -2; offsetZ <= 2; offsetZ++)
+			{
+				if (ts.z+offsetZ >= ts.zLength || ts.z+offsetZ < 0) continue;
 
 				if (ts.checkArray[ts.x+offsetX][ts.y][ts.z+offsetZ]) continue;
 
 				if (!checkOut(ts.world, ts.i+offsetX, ts.j, ts.k+offsetZ, ts.l)) continue;
 
 				scanLogs(ts.updateCoords(ts.i+offsetX, ts.j, ts.k+offsetZ, ts.x+offsetX,ts.y,ts.z+offsetZ));
-            }
-        }
-        
-        damage++;
-        if(ts.stack == null)
-        {
-            ts.world.setBlockToAir(ts.i, ts.j, ts.k);
-            dropBlockAsItem_do(ts.world, ts.i, ts.j, ts.k, new ItemStack(Item.itemsList[TFCItems.Logs.itemID],1,ts.l));
-            return;
-        }
+			}
+		}
+		
+		damage++;
+		if(ts.stack == null)
+		{
+			ts.world.setBlockToAir(ts.i, ts.j, ts.k);
+			dropBlockAsItem_do(ts.world, ts.i, ts.j, ts.k, new ItemStack(Item.itemsList[TFCItems.Logs.itemID],1,ts.l));
+			return;
+		}
 
-        if(damage+ts.stack.getItemDamage() <= ts.stack.getMaxDamage())
-        {
-            ts.world.setBlock(ts.i, ts.j, ts.k, 0, 0, 0x3);
-            if((isStone && ts.world.rand.nextInt(10) != 0) || !isStone)
-                dropBlockAsItem_do(ts.world, ts.i, ts.j, ts.k, new ItemStack(Item.itemsList[TFCItems.Logs.itemID],1,ts.l));
-        }
-    }
+		if(damage+ts.stack.getItemDamage() <= ts.stack.getMaxDamage())
+		{
+			ts.world.setBlock(ts.i, ts.j, ts.k, 0, 0, 0x3);
+			if((isStone && ts.world.rand.nextInt(10) != 0) || !isStone)
+				dropBlockAsItem_do(ts.world, ts.i, ts.j, ts.k, new ItemStack(Item.itemsList[TFCItems.Logs.itemID],1,ts.l));
+		}
+	}
 
 
 	@Override
